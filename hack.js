c7e6366607f92d58a7831aadb4e8119c50090225
Mathew Joseph <mathewjoskay@gmail.com>
2017-07-02 18:26:40 +0530

    second commit

--- hack.js
+++ hack.js
@@ -5,7 +5,7 @@
     var db = new Datastore({filename:'data.db',autoload:true});
     app.set("port",process.env.PORT||7000)
  	app.set('view engine','ejs');
- 	app.get("/home",function(req,res){
+ 	app.get("/",function(req,res){
  		res.sendFile(__dirname+"/public/home.html")
  	})
  	app.get("/login",function(req,res){
@@ -20,7 +20,7 @@
  	app.listen(app.get('port'), function () 
 		{
   			console.log('Open!!');
-		});	
+		})
  	app.get("/loginsubmit",function(req,res)
 		{
 			var pas=req.query.password;
@@ -29,14 +29,32 @@
 				"password":pas,
 				"email":em
 			}	
-			var per={}
+
 			db.find(person,function(err,result){
 
     		if(result.length!=0)
     		{
-    			db.find(per,function(err1,res1){res.render("home",{result:res1})})
+    			db.find(per,function(err1,res1){
+    				res.sendFile(__dirname+"/public/home.html")
+    			})
     			
     		}
     		else
     			res.send("No user found")
     	})	
+	app.get("/regsubmit",function(req,res)
+		{
+			var name=req.query.name;
+			var pas=req.query.password;
+			var em=req.query.email;
+			var user=req.query.username;
+			var person={
+				"name":name,
+				"password":pas,
+				"email":em,
+				"username":user
+			}
+			db.insert(person,function(err,result){
+			})
+			res.sendFile(__dirname+'/public/login.html');
+		})
